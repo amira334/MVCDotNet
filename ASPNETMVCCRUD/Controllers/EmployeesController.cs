ad65a22d05a1743465d7d06743f858c68d08a063
amira334 <amira35-334@diu.edu.bd>
2022-10-19 12:56:19 +0600

    Add project files.

---
+++ ASPNETMVCCRUD/Controllers/EmployeesController.cs
@@ -0,0 +1,60 @@
+﻿using ASPNETMVCCRUD.Data;
+using ASPNETMVCCRUD.Models.Domain;
+using ASPNETMVCCRUD.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnetmvcdatabase.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly MVCDemoDbContext mvcDemoDbContext;
+
+        public EmployeesController(MVCDemoDbContext mvcDemoDbContext)
+        {
+            this.mvcDemoDbContext = mvcDemoDbContext;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var employees = await mvcDemoDbContext.Employees.ToListAsync();
+            return View(employees);
+        }
+
+        
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeRequest)
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeRequest.Name,
+                Email = addEmployeeRequest.Email,
+                Salary = addEmployeeRequest.Salary,
+                Department = addEmployeeRequest.Department,
+                DateOfBirth = addEmployeeRequest.DateOfBirth
+
+            };
+
+            await mvcDemoDbContext.Employees.AddAsync(employee);
+            await mvcDemoDbContext.SaveChangesAsync();
+            return RedirectToAction("Add");
+
+
+        }
+
+        [HttpGet]
+        public IActionResult View(Guid id)
+        {
+            return View(id);
+        }
+    }
+}
